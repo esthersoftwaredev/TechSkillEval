3279407b9243d447d46d04f0648d042823fd0769
Esther White <monacodelisa@gmail.com>
2023-05-01 23:29:23 +0300

    header footer

--- vylentix-angular/src/app/app.module.ts
+++ vylentix-angular/src/app/app.module.ts
@@ -5,20 +5,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatButtonModule,
     MatIconModule
   ],
   providers: [],
