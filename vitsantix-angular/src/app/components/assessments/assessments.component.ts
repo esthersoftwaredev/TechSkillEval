55ba68620ff4c7f33f5040566109b3b2e7270b08
Esther White <monacodelisa@gmail.com>
2023-05-11 17:05:44 +0300

    updates

--- vitsantix-angular/src/app/components/assessments/assessments.component.ts
+++ vitsantix-angular/src/app/components/assessments/assessments.component.ts
@@ -7,17 +7,30 @@ import { Assessment } from "src/app/models/assessment";
 	styleUrls: ["./assessments.component.scss"],
 })
 export class AssessmentsComponent {
+  hoverFlags: { [key: string]: { [key: string]: boolean } } = {};
+
 	frontendSkills: Assessment[] = [
-    { icon: "html5", bgrColor: "#349edc", title: "HTML" },
-    { icon: "css3", bgrColor: "#f06529", title: "CSS" },
-    { icon: "", bgrColor: "#f06529", title: "Bootstrap" },
-    { icon: "", bgrColor: "#f06529", title: "Tailwind CSS" },
-    { icon: "", bgrColor: "#f06529", title: "SCSS" },
-    { icon: "", bgrColor: "#f06529", title: "JavaScript" },
-    { icon: "", bgrColor: "#f06529", title: "jQuery" },
-    { icon: "", bgrColor: "#f06529", title: "TypeScript" },
-    { icon: "", bgrColor: "#f06529", title: "Angular" },
-    { icon: "", bgrColor: "#f06529", title: "React" },
-    { icon: "", bgrColor: "#f06529", title: "Vue" },
-  ];
+		{ icon: "html5", bgrColor: "#349edc", title: "HTML" },
+		{ icon: "css3", bgrColor: "#f06529", title: "CSS" },
+		{ icon: "", bgrColor: "#f06529", title: "Bootstrap" },
+		{ icon: "", bgrColor: "#f06529", title: "Tailwind CSS" },
+		{ icon: "", bgrColor: "#f06529", title: "SCSS" },
+		{ icon: "", bgrColor: "#f06529", title: "JavaScript" },
+		{ icon: "", bgrColor: "#f06529", title: "jQuery" },
+		{ icon: "", bgrColor: "#f06529", title: "TypeScript" },
+		{ icon: "", bgrColor: "#f06529", title: "Angular" },
+		{ icon: "", bgrColor: "#f06529", title: "React" },
+		{ icon: "", bgrColor: "#f06529", title: "Vue" },
+	];
+
+  onButtonMouseEnter(cardIndex: number, buttonType: string) {
+    if (!this.hoverFlags[cardIndex]) {
+      this.hoverFlags[cardIndex] = {};
+    }
+    this.hoverFlags[cardIndex][buttonType] = true;
+  }
+
+  onButtonMouseLeave(cardIndex: number, buttonType: string) {
+    this.hoverFlags[cardIndex][buttonType] = false;
+  }
 }
