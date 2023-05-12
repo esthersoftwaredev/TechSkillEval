7dd6c2e499d4420a74daa4dfaa3eda19defa57bf
Esther White <monacodelisa@gmail.com>
2023-05-12 18:16:42 +0300

    itsa update

--- vitsantix-angular/src/app/models/assessment.ts
+++ vitsantix-angular/src/app/models/assessment.ts
@@ -1,17 +1,13 @@
 export interface Assessment {
   icon: string;
-  secondIcon?: string;
-  thirdIcon?: string;
-  forthIcon?: string;
-  fifthIcon?: string;
   bgrColor: string;
-  secondBgrColor?: string;
-  thirdBgrColor?: string;
-  forthBgrColor?: string;
-  fifthBgrColor?: string;
   title: string;
-  secondTitle?: string;
-  thirdTitle?: string;
-  forthTitle?: string;
-  fifthTitle?: string;
+  hasOptions?: boolean;
+  options?: Option[];
+}
+
+interface Option {
+  icon?: string;
+  bgrColor: string;
+  title: string;
 }
