c294078aec1810b44220baa5de5133cd39a2d2bb
Esther White <monacodelisa@gmail.com>
2023-05-12 18:45:37 +0300

    itsa backend done

--- vitsantix-angular/src/app/components/assessments/assessments.component.ts
+++ vitsantix-angular/src/app/components/assessments/assessments.component.ts
@@ -68,97 +68,78 @@ export class AssessmentsComponent implements OnInit {
     },
   ];
 
-	// 	{
-	// 		icon: "angular",
-	// 		secondIcon: "react",
-	// 		thirdIcon: "vue",
-	// 		forthIcon: "svelte",
-	// 		bgrColor: "#de0031",
-	// 		secondBgrColor: "#4cbad8",
-	// 		thirdBgrColor: "#41b883",
-	// 		forthBgrColor: "#ff3e00",
-	// 		title: "Angular",
-	// 		secondTitle: "React",
-	// 		thirdTitle: "Vue",
-	// 		forthTitle: "Svelte",
-	// 	},
-	// ];
+  meanStackSkills: Assessment[] = [
+    { icon: "html5", bgrColor: "#f38942", title: "HTML" },
+    {
+      icon: "css3",
+      bgrColor: "#349edc",
+      title: "CSS",
+      hasOptions: true,
+      options: [
+        { icon: "sass", bgrColor: "#cf649a", title: "SCSS" }
+      ],
+    },
+    {
+      icon: "angular",
+      bgrColor: "#de0031",
+      title: "Angular",
+    },
+    { icon: "node", bgrColor: "#3b7f3a", title: "nodeJS" },
+		{ icon: "mongodb", bgrColor: "#001e2b", title: "MongoDB" },
+  ];
 
-	// meanStackSkills: Assessment[] = [
-	// 	{ icon: "html5", bgrColor: "#f38942", title: "HTML" },
-	// 	{
-	// 		icon: "css3",
-	// 		secondIcon: "sass",
-	// 		bgrColor: "#349edc",
-	// 		secondBgrColor: "#cf649a",
-	// 		title: "CSS",
-	// 		secondTitle: "SCSS",
-	// 	},
-	// 	{
-	// 		icon: "angular",
-	// 		bgrColor: "#de0031",
-	// 		title: "Angular",
-	// 	},
-	// 	{ icon: "node", bgrColor: "#3b7f3a", title: "nodeJS" },
-	// 	{ icon: "mongodb", bgrColor: "#001e2b", title: "MongoDB" },
-	// ];
+  mernStackSkills: Assessment[] = [
+    { icon: "html5", bgrColor: "#f38942", title: "HTML" },
+    {
+      icon: "css3",
+      bgrColor: "#349edc",
+      title: "CSS",
+      hasOptions: true,
+      options: [
+        { icon: "sass", bgrColor: "#cf649a", title: "SCSS" }
+      ],
+    },
+		{
+			icon: "react",
+			bgrColor: "#4cbad8",
+			title: "React",
+		},
+    { icon: "node", bgrColor: "#3b7f3a", title: "nodeJS" },
+		{ icon: "mongodb", bgrColor: "#001e2b", title: "MongoDB" },
+  ];
 
-	// mernStackSkills: Assessment[] = [
-	// 	{ icon: "html5", bgrColor: "#f38942", title: "HTML" },
-	// 	{
-	// 		icon: "css3",
-	// 		secondIcon: "sass",
-	// 		bgrColor: "#349edc",
-	// 		secondBgrColor: "#cf649a",
-	// 		title: "CSS",
-	// 		secondTitle: "SCSS",
-	// 	},
-	// 	{
-	// 		icon: "react",
-	// 		bgrColor: "#4cbad8",
-	// 		title: "React",
-	// 	},
-	// 	{
-	// 		icon: "node",
-	// 		bgrColor: "#3b7f3a",
-	// 		title: "nodeJS",
-	// 	},
-	// 	{
-	// 		icon: "mongodb",
-	// 		bgrColor: "#001e2b",
-	// 		title: "MongoDB",
-	// 	},
-	// ];
 
-	// backendSkills: Assessment[] = [
-	// 	{
-	//     icon: "node",
-	//     secondIcon: "python",
-	//     thirdIcon: "java",
-	//     forthIcon: "go",
-	//     fifthIcon: "csharp",
-	//     bgrColor: "#3b7f3a",
-	//     secondBgrColor: "#356895",
-	//     thirdBgrColor: "#131f3f",
-	//     forthBgrColor: "#67d7e1",
-	//     fifthBgrColor: "#92458c",
-	//     title: "nodeJS",
-	//     secondTitle: "Python",
-	//     thirdTitle: "Java",
-	//     forthTitle: "Go",
-	//     fifthTitle: "C#",
-	//   },
-	// 	{ icon: "django", bgrColor: "#092e20", title: "Django" },
-	// 	{ icon: "cplusplus", bgrColor: "#659bd3", title: "C++" },
-	// 	{ icon: "dot-net", bgrColor: "#6d409d", title: ".NET" },
-	// 	{ icon: "php", bgrColor: "#4b568c", title: "PHP" },
-	// 	{ icon: "ruby", bgrColor: "#eb5745", title: "Ruby" },
-	// 	{
-	// 		icon: "mongodb",
-	// 		bgrColor: "#001e2b",
-	// 		title: "MongoDB",
-	// 	},
-	// ];
+	backendSkills: Assessment[] = [
+		{
+	    icon: "node",
+      bgrColor: "#3b7f3a",
+      title: "NodeJS",
+      hasOptions: true,
+      options: [
+        { icon: "python", bgrColor: "#356895", title: "Python" },
+        { icon: "java", bgrColor: "#131f3f", title: "Java" },
+        { icon: "go", bgrColor: "#67d7e1", title: "Go" },
+        { icon: "csharp", bgrColor: "#92458c", title: "C#" },
+      ],
+	  },
+		{
+	    icon: "php",
+      bgrColor: "#4b568c",
+      title: "PHP",
+      hasOptions: true,
+      options: [
+        { icon: "ruby", bgrColor: "#eb5745", title: "Ruby" },
+        { icon: "cplusplus", bgrColor: "#659bd3", title: "C++" },
+      ],
+	  },
+		{ icon: "django", bgrColor: "#092e20", title: "Django" },
+		{ icon: "dot-net", bgrColor: "#6d409d", title: ".NET" },
+		{
+			icon: "mongodb",
+			bgrColor: "#001e2b",
+			title: "MongoDB",
+		},
+	];
 
 	// fullstackSkills: Assessment[] = [
 	// 	{ icon: "node", bgrColor: "#3b7f3a", title: "nodeJS" },
