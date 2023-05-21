a94d4fbf2359d507e8fa36c8bacaf0c483f509e7
Esther White <monacodelisa@gmail.com>
2023-05-21 19:28:50 +0300

    mat-snack-bar

--- vitsantix-angular/src/app/app.module.ts
+++ vitsantix-angular/src/app/app.module.ts
@@ -28,7 +28,7 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
-
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
 	declarations: [
@@ -60,7 +60,8 @@ import { MatInputModule } from '@angular/material/input';
     MatRadioModule,
     MatFormFieldModule,
     MatSelectModule,
-    MatInputModule
+    MatInputModule,
+    MatSnackBarModule
 	],
 	providers: [],
 	bootstrap: [AppComponent],
--- vitsantix-angular/src/app/components/login/login.component.ts
+++ vitsantix-angular/src/app/components/login/login.component.ts
@@ -1,44 +1,60 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { UserService } from 'src/app/services/user.service';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { UserService } from "src/app/services/user.service";
+import { MatSnackBar, MatSnackBarConfig } from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+	selector: "app-login",
+	templateUrl: "./login.component.html",
+	styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent {
-  loginForm: FormGroup;
+	loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private userService: UserService) {
-    this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]]
-    });
-  }
+	constructor(
+		private formBuilder: FormBuilder,
+		private userService: UserService,
+		private snackBar: MatSnackBar
+	) {
+		this.loginForm = this.formBuilder.group({
+			email: ["", [Validators.required, Validators.email]],
+			password: ["", [Validators.required, Validators.minLength(8)]],
+		});
+	}
 
-  get email() {
-    return this.loginForm.get('email');
-  }
+	get email() {
+		return this.loginForm.get("email");
+	}
 
-  get password() {
-    return this.loginForm.get('password');
-  }
+	get password() {
+		return this.loginForm.get("password");
+	}
 
-  onSubmit() {
+	onSubmit() {
     if (this.loginForm.valid) {
       const credentials = {
         username: this.email?.value,
         password: this.password?.value
       };
 
+      const config: MatSnackBarConfig = {
+        duration: 3000,              // Set the duration (in milliseconds)
+        horizontalPosition: 'right', // Set the horizontal position to center
+        verticalPosition: 'top',      // Set the vertical position to top
+        panelClass: ['success-snackbar']  // Apply the success-snackbar CSS class
+      };
+
       this.userService.login(credentials).subscribe(
         (response) => {
           console.log('Login successful!', response);
+          config.panelClass = ['error-snackbar'];
+          this.snackBar.open('Login successful!', 'Close', config); // Display success snackbar
           // Handle successful login response here (e.g., redirect to a new page)
         },
         (error) => {
           console.log('Login failed!', error);
+          config.panelClass = ['error-snackbar']; // Apply the error-snackbar CSS class
+          this.snackBar.open('Login failed!', 'Close', config); // Display error snackbar
           // Handle login error here (e.g., display an error message)
         }
       );
