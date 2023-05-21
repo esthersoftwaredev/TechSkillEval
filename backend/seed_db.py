6a8a500c6079104d6fbd1f691ab7e693427b4896
Esther White <monacodelisa@gmail.com>
2023-05-21 16:15:26 +0300

    database changes

--- backend/seed_db.py
+++ backend/seed_db.py
@@ -2,84 +2,90 @@ from app import app, db
 from models import Assessment, Option
 
 def populate_database():
-    if not Assessment.query.filter_by(category="frontend").first():  # Check if there are any assessments already
-        # Create frontend-assessments
-        html5 = Assessment(icon="html5", bgrColor="#f38942", title="HTML", category="frontend")
-        css3 = Assessment(icon="css3", bgrColor="#349edc", title="CSS", hasOptions=True, category="frontend")
-        bootstrap = Assessment(icon="bootstrap", bgrColor="#513a75", title="Bootstrap", hasOptions=True, category="frontend")
-        jquery = Assessment(icon="jquery", bgrColor="#193556", title="jQuery", category="frontend")
-        javascript = Assessment(icon="javascript", bgrColor="#f0c022", title="JavaScript", hasOptions=True, category="frontend")
-        angular = Assessment(icon="angular", bgrColor="#de0031", title="Angular", hasOptions=True, category="frontend")
 
-        db.session.add_all([html5, css3, bootstrap, jquery, javascript, angular])
-    
-        # Create options for frontend-assessments
-        sass_option = Option(icon="sass", bgrColor="#cf649a", title="SCSS", assessment=css3, category="frontend")
-        tailwind_option = Option(icon="tailwindcss", bgrColor="#19b3ba", title="Tailwind CSS", assessment=bootstrap, category="frontend")
-        typescript_option = Option(icon="typescript", bgrColor="#2f74c0", title="TypeScript", assessment=javascript, category="frontend")
-        react_option = Option(icon="react", bgrColor="#4cbad8", title="React", assessment=angular, category="frontend")
-        vue_option = Option(icon="vue", bgrColor="#41b883", title="Vue", assessment=angular, category="frontend")
-        svelte_option = Option(icon="svelte", bgrColor="#ff3e00", title="Svelte", assessment=angular, category="frontend")
+    categories = ["frontend", "mean", "mern", "backend"]
 
-        db.session.add_all([sass_option, tailwind_option, typescript_option, react_option, vue_option, svelte_option])
-
+    for category in categories:
+        # Delete existing records for this category
+        Assessment.query.filter_by(category=category).delete()
+        Option.query.filter_by(category=category).delete()
         db.session.commit()
 
-    if not Assessment.query.filter_by(category="mean").first():
-        # Create assessments for mean-assessments
-        html5 = Assessment(icon="html5", bgrColor="#f38942", title="HTML", category="mean")
-        css3 = Assessment(icon="css3", bgrColor="#349edc", title="CSS", hasOptions=True, category="mean")
-        angular = Assessment(icon="angular", bgrColor="#de0031", title="Angular", category="mean")
-        node = Assessment(icon="node", bgrColor="#3b7f3a", title="NodeJS", category="mean")
-        mongodb = Assessment(icon="mongodb", bgrColor="#001e2b", title="MongoDB", category="mean")
+        if category == "frontend":
+            # Create frontend-assessments
+            html5 = Assessment(icon="html5", bgrColor="#f38942", title="HTML", category="frontend")
+            css3 = Assessment(icon="css3", bgrColor="#349edc", title="CSS", hasOptions=True, category="frontend")
+            bootstrap = Assessment(icon="bootstrap", bgrColor="#513a75", title="Bootstrap", hasOptions=True, category="frontend")
+            jquery = Assessment(icon="jquery", bgrColor="#193556", title="jQuery", category="frontend")
+            javascript = Assessment(icon="javascript", bgrColor="#f0c022", title="JavaScript", hasOptions=True, category="frontend")
+            angular = Assessment(icon="angular", bgrColor="#de0031", title="Angular", hasOptions=True, category="frontend")
 
-        db.session.add_all([html5, css3, angular, node, mongodb])
-
-        # Create options for mean-assessments
-        sass_option = Option(icon="sass", bgrColor="#cf649a", title="SCSS", assessment=css3, category="mean")
+            db.session.add_all([html5, css3, bootstrap, jquery, javascript, angular])
         
-        db.session.add_all([sass_option])
+            # Create options for frontend-assessments
+            sass_option = Option(icon="sass", bgrColor="#cf649a", title="SCSS", assessment=css3, category="frontend")
+            tailwind_option = Option(icon="tailwindcss", bgrColor="#19b3ba", title="Tailwind CSS", assessment=bootstrap, category="frontend")
+            typescript_option = Option(icon="typescript", bgrColor="#2f74c0", title="TypeScript", assessment=javascript, category="frontend")
+            react_option = Option(icon="react", bgrColor="#4cbad8", title="React", assessment=angular, category="frontend")
+            vue_option = Option(icon="vue", bgrColor="#41b883", title="Vue", assessment=angular, category="frontend")
+            svelte_option = Option(icon="svelte", bgrColor="#ff3e00", title="Svelte", assessment=angular, category="frontend")
 
-        db.session.commit()
+            db.session.add_all([sass_option, tailwind_option, typescript_option, react_option, vue_option, svelte_option])
+            db.session.commit()
 
-    if not Assessment.query.filter_by(category="mern").first():
-        # Create assessments for mern-assessments
-        html5 = Assessment(icon="html5", bgrColor="#f38942", title="HTML", category="mern")
-        css3 = Assessment(icon="css3", bgrColor="#349edc", title="CSS", hasOptions=True, category="mern")
-        react = Assessment(icon="react", bgrColor="#4cbad8", title="React", category="mern")
-        node = Assessment(icon="node", bgrColor="#3b7f3a", title="NodeJS", category="mern")
-        mongodb = Assessment(icon="mongodb", bgrColor="#001e2b", title="MongoDB", category="mern")
+        elif category == "mean":
+            # Create assessments for mean-assessments
+            html5 = Assessment(icon="html5", bgrColor="#f38942", title="HTML", category="mean")
+            css3 = Assessment(icon="css3", bgrColor="#349edc", title="CSS", hasOptions=True, category="mean")
+            angular = Assessment(icon="angular", bgrColor="#de0031", title="Angular", category="mean")
+            node = Assessment(icon="node", bgrColor="#3b7f3a", title="NodeJS", category="mean")
+            mongodb = Assessment(icon="mongodb", bgrColor="#001e2b", title="MongoDB", category="mean")
 
-        db.session.add_all([html5, css3, react, node, mongodb])
+            db.session.add_all([html5, css3, angular, node, mongodb])
 
-        # Create options for mern-assessments
-        sass_option = Option(icon="sass", bgrColor="#cf649a", title="SCSS", assessment=css3, category="mern")
-        
-        db.session.add_all([sass_option])
+            # Create options for mean-assessments
+            sass_option = Option(icon="sass", bgrColor="#cf649a", title="SCSS", assessment=css3, category="mean")
+            
+            db.session.add_all([sass_option])
+            db.session.commit()
 
-        db.session.commit()
+        elif category == "mern":
+            # Create assessments for mern-assessments
+            html5 = Assessment(icon="html5", bgrColor="#f38942", title="HTML", category="mern")
+            css3 = Assessment(icon="css3", bgrColor="#349edc", title="CSS", hasOptions=True, category="mern")
+            react = Assessment(icon="react", bgrColor="#4cbad8", title="React", category="mern")
+            node = Assessment(icon="node", bgrColor="#3b7f3a", title="NodeJS", category="mern")
+            mongodb = Assessment(icon="mongodb", bgrColor="#001e2b", title="MongoDB", category="mern")
 
-    if not Assessment.query.filter_by(category="backend").first():
-        # Create assessments for backend-assessments
-        node = Assessment(icon="node", bgrColor="#3b7f3a", title="NodeJS", category="backend")
-        php = Assessment(icon="php", bgrColor="#4b568c", title="PHP", hasOptions=True, category="backend")
-        django = Assessment(icon="django", bgrColor="#092e20", title="Django", hasOptions=True, category="backend")
-        mongodb = Assessment(icon="mongodb", bgrColor="#001e2b", title="MongoDB", category="backend")
+            db.session.add_all([html5, css3, react, node, mongodb])
 
-        db.session.add_all([node, php, django, mongodb])
+            # Create options for mern-assessments
+            sass_option = Option(icon="sass", bgrColor="#cf649a", title="SCSS", assessment=css3, category="mern")
+            
+            db.session.add_all([sass_option])
+            db.session.commit()
 
-        # Create options for backend-assessments
-        python_option = Option(icon="python", bgrColor="#356895", title="Python", assessment=node, category="backend")
-        java_option = Option(icon="java", bgrColor="#131f3f", title="Java", assessment=node, category="backend")
-        go_option = Option(icon="go", bgrColor="#67d7e1", title="Go", assessment=node, category="backend")
-        csharp_option = Option(icon="csharp", bgrColor="#67d7e1", title="C#", assessment=node, category="backend")
-        ruby_option = Option(icon="ruby", bgrColor="#eb5745", title="Ruby", assessment=php, category="backend")
-        cplusplus_option = Option(icon="cplusplus", bgrColor="#659bd3", title="C++", assessment=php, category="backend")
-        dotnet_option = Option(icon="dot-net", bgrColor="#6d409d", title=".NET", assessment=django, category="backend")
-        
-        db.session.add_all([python_option, java_option, go_option, csharp_option, ruby_option, cplusplus_option, dotnet_option])
+        elif category == "backend":
+            # Create assessments for backend-assessments
+            node = Assessment(icon="node", bgrColor="#3b7f3a", title="NodeJS", hasOptions=True, category="backend")
+            php = Assessment(icon="php", bgrColor="#4b568c", title="PHP", hasOptions=True, category="backend")
+            django = Assessment(icon="django", bgrColor="#092e20", title="Django", hasOptions=True, category="backend")
+            mongodb = Assessment(icon="mongodb", bgrColor="#001e2b", title="MongoDB", category="backend")
 
-        db.session.commit()
+            db.session.add_all([node, php, django, mongodb])
+
+            # Create options for backend-assessments
+            python_option = Option(icon="python", bgrColor="#356895", title="Python", assessment=node, category="backend")
+            java_option = Option(icon="java", bgrColor="#131f3f", title="Java", assessment=node, category="backend")
+            go_option = Option(icon="go", bgrColor="#67d7e1", title="Go", assessment=node, category="backend")
+            csharp_option = Option(icon="csharp", bgrColor="#67d7e1", title="C#", assessment=node, category="backend")
+            ruby_option = Option(icon="ruby", bgrColor="#eb5745", title="Ruby", assessment=php, category="backend")
+            cplusplus_option = Option(icon="cplusplus", bgrColor="#659bd3", title="C++", assessment=php, category="backend")
+            dotnet_option = Option(icon="dot-net", bgrColor="#6d409d", title=".NET", assessment=django, category="backend")
+            
+            db.session.add_all([python_option, java_option, go_option, csharp_option, ruby_option, cplusplus_option, dotnet_option])
+
+            db.session.commit()
 
 if __name__ == "__main__":
     with app.app_context():
