55224fa1b638f1caad7757a9a22feafb0996bdb2
Esther White <monacodelisa@gmail.com>
2023-05-21 18:38:20 +0300

    update

--- backend/routes.py
+++ backend/routes.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, current_app
+from flask import Blueprint
 from flask_login import login_required
 from controllers import register, login, profile,get_fe_assessments, get_mean_assessments, get_mern_assessments, get_backend_assessments, get_fullstack_assessments, get_assessment
 
 user_routes = Blueprint('users', __name__)
 
-@current_app.route('/')
+@user_routes.route('/')
 def index():
     return 'Hello, Flask! by Esther White'
 
@@ -21,26 +21,26 @@ def login_route():
 def profile_route():
     return profile()
 
-@current_app.route('/frontend-assessments', methods=['GET'])
+@user_routes.route('/frontend-assessments', methods=['GET'])
 def frontend_assessments():
     return get_fe_assessments()
 
-@current_app.route('/mean-assessments', methods=['GET'])
+@user_routes.route('/mean-assessments', methods=['GET'])
 def mean_assessments():
     return get_mean_assessments()
 
-@current_app.route('/mern-assessments', methods=['GET'])
+@user_routes.route('/mern-assessments', methods=['GET'])
 def mern_assessments():
     return get_mern_assessments()
 
-@current_app.route('/fullstack-assessments', methods=['GET'])
+@user_routes.route('/fullstack-assessments', methods=['GET'])
 def fullstack_assessments():
     return get_fullstack_assessments()
 
-@current_app.route('/backend-assessments', methods=['GET'])
+@user_routes.route('/backend-assessments', methods=['GET'])
 def backend_assessments():
     return get_backend_assessments()
 
-@current_app.route('/assessments/<assessment_id>', methods=['GET'])
+@user_routes.route('/assessments/<assessment_id>', methods=['GET'])
 def get_assessment(assessment_id):
     return get_assessment(assessment_id)
