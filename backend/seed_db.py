d9ec185d40de2a516e27455dccf951e2021824f2
Esther White <monacodelisa@gmail.com>
2023-05-22 14:09:11 +0300

    me admin added

--- backend/seed_db.py
+++ backend/seed_db.py
@@ -12,25 +12,19 @@ def populate_database():
         Option.query.filter_by(category=category).delete()
         db.session.commit()
 
-         # Check if admin user already exists
-        admin_user = User.query.filter_by(username='esteewhite').first()
-        if admin_user is None:
-            # Create admin user
-            admin_password = generate_password_hash('adminpass')
-            admin_user = User(
-                fullName='Esther White',  # Set the fullName value here
-                username='esteewhite',
-                email='esteewhite@hotmail.com',
-                password_hash=admin_password,
-                is_admin=True
-            )
-            db.session.add(admin_user)
-            db.session.commit()
-            print('Admin user created.')
-
-    if __name__ == "__main__":
-        with app.app_context():
-            populate_database()
+    # Check if admin user already exists
+    admin_user = User.query.filter_by(username='esteewhite').first()
+    if admin_user is None:
+        # Create admin user
+        admin_password = generate_password_hash('adminpass')
+        admin_user = User(
+            fullName='Esther White',  # Set the fullName value here
+            username='esteewhite',
+            email='esteewhite@hotmail.com',
+            password_hash=admin_password,
+            is_admin=True
+        )
+        db.session.add(admin_user)
 
         if category == "frontend":
             # Create frontend-assessments
